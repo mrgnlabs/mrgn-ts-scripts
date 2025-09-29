e4df9585162b9e08e0ff45425dd8a00a0f8ac192
Ilia Zyrin <ilia.zyrin.e@gmail.com>
2025-09-29 14:12:08 -0700

    more scripts

---
+++ scripts/repay.ts
@@ -0,0 +1,129 @@
+import {
+  AccountMeta,
+  ComputeBudgetProgram,
+  PublicKey,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { BN } from "@coral-xyz/anchor";
+import {
+  getAssociatedTokenAddressSync,
+} from "@mrgnlabs/mrgn-common";
+import { commonSetup } from "../lib/common-setup";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import { TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+
+const sendTx = true;
+
+type Config = {
+  PROGRAM_ID: string;
+  ACCOUNT: PublicKey;
+  BANK: PublicKey;
+  MINT: PublicKey;
+  /** In native decimals */
+  AMOUNT: BN;
+  REPAY_ALL: boolean;
+  ADD_COMPUTE_UNITS: boolean;
+
+  // Optional, omit if not using MS.
+  MULTISIG?: PublicKey;
+};
+
+const config: Config = {
+  PROGRAM_ID: "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct",
+  ACCOUNT: new PublicKey("8HwNc1W9YUydGWLFiyahwrUAWz5yuW7gSiPHaU6sSFSr"),
+  BANK: new PublicKey("64NtNrDgwY4U8ktazsPMNSBNdwjeFgxauqxi9f6u9ym8"),
+  MINT: new PublicKey("DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"),
+  AMOUNT: new BN(10 * 10 ** 6),
+  REPAY_ALL: true,
+  ADD_COMPUTE_UNITS: false,
+};
+
+async function main() {
+  const user = commonSetup(
+    sendTx,
+    config.PROGRAM_ID,
+    "/keys/staging-deploy.json",
+    config.MULTISIG,
+    "current"
+  );
+  const program = user.program;
+  const connection = user.connection;
+
+  let mintAccInfo = await connection.getAccountInfo(config.MINT);
+  const tokenProgram = mintAccInfo.owner;
+  let isT22 = tokenProgram.toString() == TOKEN_2022_PROGRAM_ID.toString();
+
+  let meta: AccountMeta[] = [];
+
+  if (isT22) {
+    const m: AccountMeta = {
+      pubkey: config.MINT,
+      isSigner: false,
+      isWritable: false,
+    };
+    // must be pushed first in the array
+    meta.unshift(m);
+  }
+
+  const ata = getAssociatedTokenAddressSync(
+    config.MINT,
+    user.wallet.publicKey,
+    true,
+    tokenProgram
+  );
+  const transaction = new Transaction();
+
+  if (config.ADD_COMPUTE_UNITS) {
+    transaction.add(
+      ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 100_000 })
+    );
+    transaction.add(
+      ComputeBudgetProgram.setComputeUnitLimit({ units: 1_400_000 })
+    );
+  }
+
+  transaction.add(
+    await program.methods
+      .lendingAccountRepay(config.AMOUNT, config.REPAY_ALL)
+      .accounts({
+        marginfiAccount: config.ACCOUNT,
+        bank: config.BANK,
+        signerTokenAccount: ata,
+        tokenProgram: tokenProgram,
+      })
+      .remainingAccounts(meta)
+      .instruction()
+  );
+
+  console.log(
+    "repaying : " + config.AMOUNT.toString() + " to " + config.BANK
+  );
+
+  if (sendTx) {
+    try {
+      const signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [user.wallet.payer]
+      );
+      console.log("Transaction signature:", signature);
+    } catch (error) {
+      console.error("Transaction failed:", error);
+    }
+  } else {
+    transaction.feePayer = config.MULTISIG; // Set the fee payer to Squads wallet
+    const { blockhash } = await connection.getLatestBlockhash();
+    transaction.recentBlockhash = blockhash;
+    const serializedTransaction = transaction.serialize({
+      requireAllSignatures: false,
+      verifySignatures: false,
+    });
+    const base58Transaction = bs58.encode(serializedTransaction);
+    console.log("Base58-encoded transaction:", base58Transaction);
+  }
+}
+
+main().catch((err) => {
+  console.error(err);
+});
