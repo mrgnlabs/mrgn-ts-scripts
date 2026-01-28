aac48e43887895ca1caa6fa4809b6261a14f9d09
jgur-psyops <jonathan@mrgn.group>
2026-01-27 22:48:53 -0500

    drift bank initial deploy

--- luts/update_lut.ts
+++ luts/update_lut.ts
@@ -28,28 +28,34 @@ type Config = {
 };
 
 const config: Config = {
-  LUT: new PublicKey("CQ8omkUwDtsszuJLo9grtXCeEyDU4QqBLRv9AjRDaUZ3"),
+  LUT: new PublicKey("FtQ5uKQvFoKQ27SWY15tgBeJQnGKmKGzWqDz7kGUbeiq"),
   KEYS: [
-    new PublicKey("Dso1bDeDjCQxTrWHqUUi63oBvV7Mdm6WaobLbQ7gnPQ"),
-    new PublicKey("AEq1mcpesN4u9CSd8uUQbQar6qLghNQx7rnhdQhAnUyn"),
-    // new PublicKey("E1dNntvZo6pXmkZq43wommAdxe6qF1wcXkjz2M2QEb14"),
-    // new PublicKey("AHTqUF4LyDxCnpHWL89RjsJWGEGXKSYyQPWx3fUAcj1H"),
-    // new PublicKey("8nUGEsT5VJijkpnn6fJXWTyyZjExhwipFuDyotqwyzhz"),
-    // new PublicKey("89LuR6urx9wMxeJtf3LCdq84LsgM22Sp6fWqPbCuZtUr"),
-    // new PublicKey("74KM1fwNm9WP39UH7QsCs4dvkN6RaZT52U9f4tnkJtom"),
-    // new PublicKey("5LTAowCUEK5rr2ALKtk6cTHhyaPTCbksV5C3pCqLKSVu"),
-    // new PublicKey("5HxHAW3BCYPB2uRMrjKpgA6mSpwHPK1JrntWZU4QZpZ1"),
+    // new PublicKey("Dso1bDeDjCQxTrWHqUUi63oBvV7Mdm6WaobLbQ7gnPQ"),
+    // new PublicKey("AEq1mcpesN4u9CSd8uUQbQar6qLghNQx7rnhdQhAnUyn"),
+
+    new PublicKey("4qp6Fx6tnZkY5Wropq9wUYgtFxXKwE6viZxFHg3rdAG8"),
+    new PublicKey("CwbdVVmGfR4H9bQ39vmeKjt9Gd128iFGrc9Jvht2oPx6"),
+    new PublicKey("3x85u7SWkmmr7YQGYhtjARgxwegTLJgkSLRprfXod6rh"),
+    new PublicKey("5kLrcBhm66VLvGKT4my82uSKG5vQyAcyQADgnQ4rinLi"),
+    new PublicKey("7RiMUsA8KaSkq2KzZPSKAsSyXZ38PCXbVrWgh468dcsd"),
+    new PublicKey("CtFbocisXmWnTvCUEqZHGwW4MwyAsZxLZGrhLhn9BpK2"),
+    new PublicKey("Fx2zxMQsL3Dck7j8GAG4397Rc7woRezJPAhHumphP1e"),
+    new PublicKey("W1Hja1hhpzFy16WUCcQcnCZxuo5xThNex9c41vXM2DF"),
+    new PublicKey("EADW8iFEsyfuXuBjETVE27kK5yrpfJNqJ5usgm3ehinH"),
+    new PublicKey("5pBHde9B7n8dZoLTGB4N9WWBawJ8BeQdj17QkrqXXtrr"),
+    new PublicKey("GZ8GGS89K6XUDTRESNjWvgRkvsMui6TZn689c6E28Fe4"),
+    new PublicKey("4Hmd6PdjVA9auCoScE12iaBogfwS4ZXQ6VZoBeqanwWW"),
   ],
 };
 
 async function main() {
-  await updateLut(sendTx, config, "/keys/phantom-wallet.json");
+  await updateLut(sendTx, config, "/.keys/staging-lut-admin.json");
 }
 
 export async function updateLut(
   sendTx: boolean,
   config: Config,
-  walletPath: string
+  walletPath: string,
 ) {
   loadEnvFile(".env.api");
   const apiUrl = process.env.API_URL || DEFAULT_API_URL;
@@ -67,16 +73,16 @@ export async function updateLut(
   // Extract the existing addresses from the lookup table
   const existingAddresses = lutAccount.value.state.addresses;
   const existingSet = new Set(
-    existingAddresses.map((addr: PublicKey) => addr.toBase58())
+    existingAddresses.map((addr: PublicKey) => addr.toBase58()),
   );
 
   // Filter out keys that are already in the lookup table
   const keysToAdd = config.KEYS.filter(
-    (key) => !existingSet.has(key.toBase58())
+    (key) => !existingSet.has(key.toBase58()),
   );
   if (keysToAdd.length === 0) {
     console.log(
-      "No new keys to add, lookup table is already up to date, aborting."
+      "No new keys to add, lookup table is already up to date, aborting.",
     );
     return;
   } else {
@@ -101,7 +107,7 @@ export async function updateLut(
       const signature = await sendAndConfirmTransaction(
         connection,
         transaction,
-        [wallet]
+        [wallet],
       );
       console.log("Transaction signature:", signature);
     } catch (error) {
--- scripts/fetch-bank.ts
+++ scripts/fetch-bank.ts
@@ -24,7 +24,7 @@ const config: Config = {
     // new PublicKey("HmpMfL8942u22htC4EMiWgLX931g3sacXFR6KjuLgKLV"), // usdt
     // new PublicKey("8UEiPmgZHXXEDrqLS3oiTxQxTbeYTtPbeMBxAd2XGbpu"), // py
     // new PublicKey("FDsf8sj6SoV313qrA91yms3u5b3P4hBxEPvanVs8LtJV"), // usds
-    new PublicKey("2ZScBCNKfE6X6fGcBtB2uBuvZqUE3cjKQmRv8wVXTN5B"),
+    new PublicKey("8W3GgWFFnHdd98GKGzvNNi9Wzjoq2CU4wW6cHz6cKxk1"),
   ],
 };
 
