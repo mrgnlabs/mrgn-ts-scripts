f176a1bb2f1cbbcf2558121c2eb473154472b918
jgur-psyops <jonathan@mrgn.group>
2026-02-26 13:41:36 -0500

    Add eurc Feb 26 2025

--- luts/update_lut.ts
+++ luts/update_lut.ts
@@ -28,9 +28,9 @@ type Config = {
 const config: Config = {
   LUT: new PublicKey("CQ8omkUwDtsszuJLo9grtXCeEyDU4QqBLRv9AjRDaUZ3"),
   KEYS: [
-    new PublicKey("GJ3CvAnawvxwReu7MhBfTQBLcfcs1awP5h55SzoBBNYq"),
-    new PublicKey("EcL93EQPJUgGehECHKhQ96cs6LWJXktzf7to92d9AjCT"),
-    new PublicKey("5KjGKr7pKBG7DLcPdZTiSji7cwNYJ5ERj2pPSNLTejoZ"),
+    new PublicKey("D7xjUTY2tH1QWd7cefecJy86DTWJaWRj6XevzChGDzXm"),
+    new PublicKey("CTn1eaLioKeZ53DyA5XRYVA9rWutxCfTzfvyezAPuSuC"),
+    new PublicKey("HzwqbKZw8HxMN6bF2yFZNrht3c2iXXzpKcFu7uBEDKtr"),
     // new PublicKey("Dpw1EAVrSB1ibxiDQyTAW6Zip3J4Btk2x4SgApQCeFbX"),
     // new PublicKey("6gMq3mRCKf8aP3ttTyYhuijVZ2LGi14oDsBbkgubfLB3"),
     // new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
